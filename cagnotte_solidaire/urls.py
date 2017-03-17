cdb98ac0d18e2acc1fe7a05539af6d5700f7ff99
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-03-17 18:02:14 +0100

    medias, admin, projet_detail

--- cagnotte_solidaire/urls.py
+++ cagnotte_solidaire/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^', include('projets.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- projets/admin.py
+++ projets/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+from django.contrib.admin import site
 
-# Register your models here.
+from .models import Projet, Proposition, Offre
+
+
+site.register(Projet)
+site.register(Proposition)
+site.register(Offre)
