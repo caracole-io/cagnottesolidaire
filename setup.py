b1c3cec263004612152f7e69f450c9cc1d19364c
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-09-10 20:43:28 +0200

    fix install

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 
 from setuptools import setup
@@ -8,6 +10,8 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 with open(os.path.join(os.path.dirname(__file__), 'requirements.in')) as requirements:
     REQUIREMENTS = requirements.readlines()
 
+REQUIREMENTS_LINK = [req.split()[1] for req in REQUIREMENTS if '://' in req]
+REQUIREMENTS_PYPI = [req for req in REQUIREMENTS if '://' not in req]
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
@@ -16,7 +20,8 @@ setup(
     name='cagnottesolidaire',
     version='1.0.0',
     packages=['cagnottesolidaire'],
-    install_requires=REQUIREMENTS,
+    install_requires=REQUIREMENTS_PYPI,
+    dependency_links=REQUIREMENTS_LINK,
     include_package_data=True,
     license='GPL License',
     description='A social crowdfunding app',
@@ -31,8 +36,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
