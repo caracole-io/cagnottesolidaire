ce0a3c782b277f1ad015bc73dc26bbf828e29741
Guilhem Saurel <guilhem.saurel@gmail.com>
2019-02-24 09:11:28 +0100

    make tests pass for one more year…

--- testproject/tests.py
+++ testproject/tests.py
@@ -39,7 +39,7 @@ class TestCagnotte(TestCase):
         self.assertEqual(Cagnotte.objects.count(), 0)
         self.assertEqual(r.status_code, 200)
         # fin_achat < fin_depot
-        cagnotte_data['fin_depot'] = '31/12/2018'
+        cagnotte_data['fin_depot'] = '31/12/2019'
         self.assertLess(strpdate(cagnotte_data['fin_achat']), strpdate(cagnotte_data['fin_depot']))
         r = self.client.post(reverse('cagnottesolidaire:cagnotte_create'), cagnotte_data)
         self.assertEqual(Cagnotte.objects.count(), 0)
@@ -97,7 +97,7 @@ class TestCagnotte(TestCase):
             objectif='nothing',
             finances=43,
             fin_depot=date(2017, 12, 31),
-            fin_achat=date(2018, 12, 31))
+            fin_achat=date(2019, 12, 31))
         prop = Proposition.objects.create(name='Pipo', description='nope', prix=20, cagnotte=proj, responsable=guy)
         propd = {'p_slug': proj.slug, 'slug': prop.slug}
         self.assertEqual(self.client.get(reverse('cagnottesolidaire:proposition', kwargs=propd)).status_code, 200)
@@ -195,7 +195,7 @@ class TestCagnotte(TestCase):
             objectif='nothing',
             finances=43,
             fin_depot=date(2017, 12, 31),
-            fin_achat=date(2018, 12, 31))
+            fin_achat=date(2019, 12, 31))
         prop = Proposition.objects.create(
             name='Pipo', description='nope', prix=20, cagnotte=proj, responsable=b, beneficiaires=2)
         self.client.login(username='c', password='c')
